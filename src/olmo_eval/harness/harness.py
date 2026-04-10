7f9991011f42d9af9d20dfdd7f77067884f81faf
mgmorgan23 <maliam@allenai.org>
2026-04-10 10:48:19 -0700

    test backend

--- src/olmo_eval/harness/harness.py
+++ src/olmo_eval/harness/harness.py
@@ -317,19 +317,20 @@ class Harness:
             New request with config applied.
         """
 
-        from olmo_eval.common.types import RequestType
+        # from olmo_eval.common.types import RequestType
 
         messages = self._inject_system_prompt(request.messages)
 
-        if request.request_type == RequestType.CHAT:
-            from olmo_eval.common.inspection import format_with_chat_template
+        # if request.request_type == RequestType.CHAT:
+        #     from olmo_eval.common.inspection import format_with_chat_template
 
-            formatted_prompt = format_with_chat_template(request, self.provider.get_tokenizer())
+        #     formatted_prompt = format_with_chat_template(request, self.provider.get_tokenizer())
 
         return LMRequest(
             request_type=request.request_type,
             messages=messages,
-            prompt=formatted_prompt or request.prompt,
+            # prompt=formatted_prompt or request.prompt,
+            prompt=request.prompt,
             continuations=request.continuations,
             tools=self.config.tool_schemas if self.config.has_tools else request.tools,
             system_prompt=self.config.system_prompt or request.system_prompt,
