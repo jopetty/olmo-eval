04774ded90b1815a8a22b31352d1bdc2b89c3f0f
Jackson Petty <jacksonp@allenai.org>
2026-07-23 08:29:51 -0700

    retokens

--- local/run_state_bench_lc.py
+++ local/run_state_bench_lc.py
@@ -3,7 +3,7 @@ import json
 import subprocess
 import time
 
-GROUP = "jacksonp-state-bench-lc-6"
+GROUP = "jacksonp-state-bench-lc-7"
 DEFAULT_CLUSTER = "ai2/saturn"
 NUM_GPUS = 4
 WORKSPACE = "ai2/beyond-state"
